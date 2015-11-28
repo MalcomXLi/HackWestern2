1ded4334b6968cb0099e27d0a3517596f30a80a7
MalcomXLi <malcom309@gmail.com>
2015-11-28 02:00:33 -0800

    possible twilio response

--- app.js
+++ app.js
@@ -3,12 +3,12 @@ var app = express ();
 var bodyParser = require("body-parser");
 var twilio = require("twilio");
 
+
 app.set('port', (process.env.PORT || 3000));
 
 //Serving files, such as images, CSS, JavaScript and other static files 
 app.use('/', express.static(__dirname+ '/public'));
 app.use(bodyParser.json());
-
 
 var server = app.listen(app.get("port"), function () {
     var host = server.address().address;
@@ -16,3 +16,10 @@ var server = app.listen(app.get("port"), function () {
 
     console.log('Example app listening at http://%s:%s', host, port);
 });
+
+app.post('/', twilio.webhook(), function(request, response) {
+    var twiml = new twilio.TwimlResponse();
+    twiml.message('This HTTP request came from Twilio!');
+    response.send(twiml);
+});
+
