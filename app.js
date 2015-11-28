5f8237875eb4669937df154a0fd2310932722292
MalcomXLi <malcom309@gmail.com>
2015-11-28 12:20:50 -0800

    lemme test this

--- app.js
+++ app.js
@@ -2,45 +2,84 @@ var express = require("express");
 var app = express ();
 var bodyParser = require("body-parser");
 var twilio = require("twilio");
-var wolfram = require('./wolfram.js');
+//var wolfram = require('./wolfram.js');
 
 app.set('port', (process.env.PORT || 3000));
 
-//Serving files, such as images, CSS, JavaScript and other static files 
-app.use('/', express.static(__dirname+ '/public'));
+// Create an express web app
+var app = express();
+
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+	extended: true
+}));
 
-var server = app.listen(app.get("port"), function () {
-    var host = server.address().address;
-    var port = server.address().port;
 
-    console.log('Example app listening at http://%s:%s', host, port);
-});
-
-app.post('/sms', twilio.webhook({
-    validate:false
-}), function(request, response) {
-	console.log(request);
+app.post('/sms',twilio.webhook(), function(request, response) {
+    // Create a TwiML response
     var twiml = new twilio.TwimlResponse();
-    twiml.message('What is UP my NIGGA');
-    response.send(twiml);
+    twiml.message('HOLY SHITTT WTF!');
+    
+    response.send(twiml;
 });
 
+app.listen(process.env.PORT || 3000);
 
-app.get('/wolfram', function(request, response){
-    response.set({
-        "Content-Type": "application/json",
-        "Access-Control-Allow-Origin": "*"
-    });
 
-    var query = "integrate 2x";
 
-    wolfram.queryWolfram(query, function(err, result){
-        if (err){
-            response.status(404).send(err);
-        }
-        else{
-            response.status(200).send(result);      
-        }
-    });
-});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Serving files, such as images, CSS, JavaScript and other static files 
+// app.use('/', express.static(__dirname+ '/public'));
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({
+// 	extended: true
+// }));
+
+// var server = app.listen(app.get("port"), function () {
+//     var host = server.address().address;
+//     var port = server.address().port;
+
+//     console.log('Example app listening at http://%s:%s', host, port);
+// });
+
+// app.post('/sms', twilio.webhook(), function(request, response) {
+
+// 	console.log(request);
+//     var twiml = new twilio.TwimlResponse();
+//     twiml.message('What is UP my NIGGA');
+//     response.send(twiml);
+// });
+
+
+// app.get('/wolfram', function(request, response){
+//     response.set({
+//         "Content-Type": "application/json",
+//         "Access-Control-Allow-Origin": "*"
+//     });
+
+//     var query = "integrate 2x";
+
+//     wolfram.queryWolfram(query, function(err, result){
+//         if (err){
+//             response.status(404).send(err);
+//         }
+//         else{
+//             response.status(200).send(result);      
+//         }
+//     });
+// });
