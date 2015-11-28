af10941d885f8931f254e1a880d7fb93f6e17fc1
MalcomXLi <malcom309@gmail.com>
2015-11-28 12:34:40 -0800

    authentication fix2

--- app.js
+++ app.js
@@ -30,12 +30,7 @@ app.use(bodyParser.urlencoded({
 	extended: true
 }));
 
-var server = app.listen(app.get("port"), function () {
-    var host = server.address().address;
-    var port = server.address().port;
-
-    console.log('Example app listening at http://%s:%s', host, port);
-});
+app.listen(process.env.PORT || 3000);
 
 app.post('/sms', twilio.webhook(), function(request, response) {
 
